f0f4c42a7143a83224ac065eda9144927641dcfb
NikkWalker <example@gmail.com>
2021-01-12 17:50:02 +0700

    add h4

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+
+let a = 10;
+console.log(a);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
